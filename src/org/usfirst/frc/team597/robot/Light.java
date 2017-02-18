657b80343a6b9fe0b7e107ba3c5a3e4ecdadd300
chromefox4 <chromefox4>
2017-02-18 14:26:30 -0800

    Finished all my classes.

--- src/org/usfirst/frc/team597/robot/Light.java
+++ src/org/usfirst/frc/team597/robot/Light.java
@@ -1,7 +1,37 @@
 package org.usfirst.frc.team597.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.VictorSP;
 
 public class Light {
 Relay light;
+public Light(Relay led) {
+	light = led;
+//Name 
+}
+
+public void LightClassON() {
+	light.set(Relay.Value.kForward);
+}
+public void LightClassOFF() {	
+	light.set(Relay.Value.kOff);
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
