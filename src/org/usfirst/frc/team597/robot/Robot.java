ec10af38e909ee16909efe0c42abd0ab9b0811be
lausd_user <lausd_user@WINDOWS-LIJPRMR>
2017-02-18 16:14:22 -0800

    cleaned up a bit

--- src/org/usfirst/frc/team597/robot/Robot.java
+++ src/org/usfirst/frc/team597/robot/Robot.java
@@ -1,10 +1,7 @@
 package org.usfirst.frc.team597.robot;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
